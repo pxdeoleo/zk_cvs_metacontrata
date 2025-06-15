9254d78ff64fb7a94f67bc06893fd46c0cf8972a
pdeoleo <pedrodeoleoc@gmail.com>
2025-06-15 19:30:45 -0400

    feat: update employees; update status

--- main.py
+++ main.py
@@ -128,63 +128,80 @@ async def sync_employees(meta_client: MetaContrataClient, cv_employees_api: CVSe
     meta_employees = meta_employees_task.result()
     cv_employees = cv_employees_task.result()
 
-    meta_emp_codes: set[str] = {str(emp['coInEmpl']) for emp in meta_employees}
-    cv_emp_codes: set[str] = {str(emp['pin']) for emp in cv_employees}
+    meta_by_code = {str(emp["coInEmpl"]): emp for emp in meta_employees}
+    cv_by_code = {str(emp["pin"]): emp for emp in cv_employees}
 
-    new_employees = [emp for emp in meta_employees if str(emp['coInEmpl']) not in cv_emp_codes]
-    new_cv_employees = []
-    for emp in new_employees:
-        first_name, last_name = normalize_full_name(emp['nombre'], emp['apellido1'] + ' ' + emp['apellido2'])
-        new_cv_employees.append(Person(pin=str(emp['coInEmpl']),
-                                       deptCode=str(emp['coInSub']),
-                                       name=first_name,
-                                       lastName=last_name))
+    meta_emp_codes = set(meta_by_code.keys())
+    cv_emp_codes = set(cv_by_code.keys())
 
-    # cv_create_emp_task = asyncio.create_task(cv_employees_api.bulk_add_or_edit_persons(new_cv_employees))
-    logging.info(f"Starting the creation of {len(new_employees)} employees")
-    for i, emp_batch in enumerate(generate_batch(new_cv_employees, 100)):
-        logging.info(f"Batch {i + 1} / {int(len(new_cv_employees) / 100)}")
-        await cv_employees_api.bulk_add_or_edit_persons(emp_batch)
+    # Determine new, deleted, and common employees
+    new_codes = meta_emp_codes - cv_emp_codes
+    deleted_codes = cv_emp_codes - meta_emp_codes
+    common_codes = meta_emp_codes & cv_emp_codes
 
-        del_employees: list[str] = [str(emp['pin']) for emp in cv_employees if emp['pin'] not in meta_emp_codes]
+    # --- CREATE ---
+    to_create: list[Person] = []
 
-    cv_delete_emp_task = cv_employees_api.bulk_delete_persons_by_pin(del_employees)
+    if len(to_create) > 0:
+        for code in new_codes:
+            emp = meta_by_code[code]
+            first_name, last_name = normalize_full_name(emp["nombre"], f'{emp["apellido1"]} {emp["apellido2"]}')
+            to_create.append(Person(
+                pin=code,
+                deptCode=str(emp["coInSub"]),
+                name=first_name,
+                lastName=last_name
+            ))
 
-    # await cv_create_emp_task
-    await cv_delete_emp_task
-    # for emp in meta_employees:
-    #     if emp["coInEmpl"] not in cv_emp_codes:
-    #         new_persons.append(Person(
-    #             pin = code,
-    #             first emp["nombre"],
-    #             "lastName": f"{emp['apellido1']} {emp['apellido2']}".strip(),
-    #             "isDisabled": emp["accesoPermitido"] == 0
-    #         ))
+        logger.info(f"Creating {len(to_create)} new employees")
+        for i, batch in enumerate(generate_batch(to_create, 100)):
+            logger.info(f"Create batch {i + 1}")
+            await cv_employees_api.bulk_add_or_edit_persons(batch)
 
-    # Handle deletions
-    # for code in cv_emp_codes - meta_emp_codes:
-    #     cv_client.delete_employee(code)
-    #
-    # # Handle updates
-    # for emp in meta_employees:
-    #     code = emp["coInEmpl"]
-    #     if code in cv_emp_codes:
-    #         existing = cv_employees_code[code]
-    #         full_last_name = f"{emp['apellido1']} {emp['apellido2']}".strip()
-    #         need_update = (
-    #                 normalize_name(existing["firstName"]) != normalize_name(emp["nombre"]) or
-    #                 normalize_name(existing["lastName"]) != normalize_name(full_last_name)
-    #         )
-    #         if need_update:
-    #             cv_client.update_employee({
-    #                 "employeeCode": code,
-    #                 "firstName": emp["nombre"],
-    #                 "lastName": full_last_name,
-    #             })
-    #
-    #         access_allowed = emp["accesoPermitido"] == 1
-    #         if existing["accessEnabled"] != access_allowed:
-    #             cv_client.set_access(code, access_allowed)
+        # Update after creation
+        cv_employees = await cv_employees_api.get_all_persons()
+        cv_by_code = {str(emp["pin"]): emp for emp in cv_employees}
+        cv_emp_codes = set(cv_by_code.keys())
+        common_codes = meta_emp_codes & cv_emp_codes
+
+    # --- UPDATE ---
+    to_update: list[Person] = []
+    for code in common_codes:
+        meta_emp = meta_by_code[code]
+        cv_person = cv_by_code[code]
+
+        first_name, last_name = normalize_full_name(meta_emp["nombre"],
+                                                    f'{meta_emp["apellido1"]} {meta_emp["apellido2"]}')
+        access_allowed = bool(int(meta_emp.get("accesoPermitido", "0")))
+        current_allowed = True if None == cv_person.get("isDisabled", "0") or "0" else False
+
+        if (
+                first_name != cv_person.get("name") or
+                last_name != cv_person.get("lastName") or
+                access_allowed != current_allowed
+        ):
+            to_update.append(Person(
+                pin=code,
+                deptCode=str(meta_emp["coInSub"]),
+                name=first_name,
+                lastName=last_name,
+                isDisabled=not access_allowed
+            ))
+
+    if len(to_update) > 0:
+        logger.info(f"Updating {len(to_update)} existing employees")
+        for i, batch in enumerate(generate_batch(to_update, 100)):
+            logger.info(f"Update batch {i + 1}")
+            await cv_employees_api.bulk_add_or_edit_persons(batch)
+
+    # --- DELETE ---
+    if len(deleted_codes) > 0:
+        to_delete: list[str] = list(deleted_codes)
+
+        logger.info(f"Deleting {len(to_delete)} obsolete employees")
+        for i, batch in enumerate(generate_batch(to_delete, 100)):
+            logger.info(f"Delete batch {i + 1}")
+            await cv_employees_api.bulk_delete_persons_by_pin(batch)
 
 
 async def main() -> None:
